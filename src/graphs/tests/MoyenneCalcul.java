127709c3aa38964f0129f0aa68f09ccdf21b5175
Léopold V <5019-leopold.varlamoff.etu@users.noreply.gitlab.univ-lille.fr>
2022-03-30 11:09:23 +0200

    salut!!!!!!!!!!

--- src/graphs/tests/MoyenneCalcul.java
+++ src/graphs/tests/MoyenneCalcul.java
@@ -10,43 +10,56 @@ import fr.ulille.but.sae2_02.graphes.GrapheNonOrienteValue;
  * Classe pour tester une affectation avec un calcul utilisant les moyennes
  */
 public class MoyenneCalcul {
+    // tableau étudiants à aider
+    private static String[][] partieN = new String[][]{{"Jean","8.63","1"},{"Harry","7.97","1"},{"Paul","10.26","1"},{"Timothé","9.81","1"}};
+    
+    // tableau étudiants tutorat jsp quoi 
+    private static String[][] partieF = new String[][]{{"Antoine","13.64","3"},{"Romain","14.12","3"},{"Florence","16.39","2"},{"Camille","14.54","2"}};
+
+    // listes
+    ArrayList<Etudiant> listEtuN = new ArrayList<>();
+    ArrayList<Etudiant> listEtuF = new ArrayList<>();
+
     public static void main(String[] args) {
-        // étudiants à aider (partieN)
-        String[][] partieN = new String[][]{{"Jean","8.63","1"},{"Harry","7.97","1"},{"Paul","10.26","1"},{"Timothé","9.81","1"}};
+        MoyenneCalcul ok = new MoyenneCalcul();
 
-        // étudiants aideurs (partieF)
-        String[][] partieF = new String[][]{{"Antoine","13.64","3"},{"Romain","14.12","3"},{"Florence","16.39","2"},{"Camille","14.54","2"}};
-
-        // on les fout dans des listes pour plus tard (trust me)
-        List<String> partieNList = new ArrayList<>();
         for (String[] strings : partieN) {
-            partieNList.add(strings[0]);
+            ok.listEtuN.add(new Etudiant(strings[0], Double.parseDouble(strings[1]), Integer.parseInt(strings[2])));
+        }
+        for (String[] strings : partieF) {
+            ok.listEtuF.add(new Etudiant(strings[0], Double.parseDouble(strings[1]), Integer.parseInt(strings[2])));
         }
 
-        List<String> partieFList = new ArrayList<>();
-        for (String[] strings : partieF) {
-            partieFList.add(strings[0]);
+        // liste de nom parce que c'est de la merde
+        List<String> nomsN = new ArrayList<>();
+        List<String> nomsF = new ArrayList<>();
+
+        for (Etudiant etudiant : ok.listEtuN) {
+            nomsN.add(etudiant.nom);
+        }
+        for (Etudiant etudiant : ok.listEtuF) {
+            nomsF.add(etudiant.nom);
         }
 
         // nouveau graph
         GrapheNonOrienteValue<String> graph = new GrapheNonOrienteValue<>();
 
         // on ajoute tous les sommets (utiliser la liste à la place plus tard)
-        for (String[] strings : partieN) {
-            graph.ajouterSommet(strings[0]);
+        for (String string : nomsN) {
+            graph.ajouterSommet(string);
         }
-        for (String[] strings : partieF) {
-            graph.ajouterSommet(strings[0]);
+        for (String string : nomsF) {
+            graph.ajouterSommet(string);
         }
 
         // on fait un savant calcul pour faire les arêtes
         double calculN, calculF, total;
-        for (String[] stringsN : partieN) {
-            for (String[] stringsF : partieF) {
-                calculN = Double.parseDouble(stringsN[1]) * Integer.parseInt(stringsN[2]);
-                calculF = Double.parseDouble(stringsF[1]) * Integer.parseInt(stringsF[2]);
+        for (Etudiant etudiantN : ok.listEtuN) {
+            for (Etudiant etudiantF : ok.listEtuF) {
+                calculN = etudiantN.moyenne * etudiantN.niveau;
+                calculF = etudiantF.moyenne * etudiantF.niveau;
                 total = calculF - calculN;
-                graph.ajouterArete(stringsN[0], stringsF[0], total);
+                graph.ajouterArete(etudiantN.nom, etudiantF.nom, total);
             }
         }
 
@@ -54,7 +67,7 @@ public class MoyenneCalcul {
         System.out.println(graph.toString() + "\n");
 
         // on fait une affectation
-        CalculAffectation<String> affectation = new CalculAffectation<>(graph, partieNList, partieFList);
+        CalculAffectation<String> affectation = new CalculAffectation<>(graph, nomsN, nomsF);
 
         // pour check
         System.out.println(affectation.getAffectation() + "\n");
@@ -63,4 +76,6 @@ public class MoyenneCalcul {
 
     // faire un classement : partieN = plus mauvais en premier et partieF = meilleur en premier
     // poids des arêtes = classement partieF - classement partieN
+
+    // probablement de la grosse merde : voir la classe Rang pour un meilleur truc :)
 }
