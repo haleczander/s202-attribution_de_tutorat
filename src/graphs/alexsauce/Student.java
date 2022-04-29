bd2d5d8d49a3cc4c5422b8efde5228a432a62ebb
haleczander <95761609+haleczander@users.noreply.github.com>
2022-04-29 21:20:01 +0200

    alex : autre version de modelisation

---
+++ src/graphs/alexsauce/Student.java
@@ -0,0 +1,72 @@
+package alexsauce;
+
+/**Classe abstraite student comprenant
+ * un nom
+ * une moyenne
+ * une année
+ */
+public abstract class Student implements Comparable<Student> {
+    private String name;
+    private double grades;
+    private int level;
+    private double weight;
+
+    private static boolean toStringShort=false;
+
+    
+    
+    public Student(String name, double grades, int level) {
+        this.name = name;
+        this.grades = grades;
+        this.level = level;
+        this.weight = computeWeight();
+    }
+
+    /**Getters seulement */
+    public String getName() {
+        return name;
+    }
+
+    public double getGrades() {
+        return grades;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    /**Setter du poids, pour forcer une affectation */
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    /**Setter de la fonction d'affichage réduite */
+    public void setToStringShort(){
+        Student.toStringShort=true;
+    }
+
+    
+    /** toString() avec 
+     * un mode court : retourne this.name
+     * un mode long : nom + level + notes
+     */
+    @Override
+    public String toString() {
+        if (Student.toStringShort) { return this.getName(); }
+        return "Student [name=" + this.name + ", level=" + this.level + ", grades=" + this.grades  + "]";
+    }
+    
+    /**Fonction du calcul du poids, 
+     * à définir selon tuteur ou tutoré  
+     */
+    private abstract double computeWeight();
+
+    /**Compare deux étudiants en fonction de leurs poids respectifs */
+    public int compareTo(Student o){
+        return (int)((this.weight-o.getWeight())*100);
+    }
+
+}
---
+++ src/graphs/alexsauce/Tutored.java
@@ -0,0 +1,14 @@
+package alexsauce;
+
+public class Tutored extends Student{
+
+    public Tutored(String name, double grades, int level) {
+        super(name, grades, level);
+    }
+
+    @Override
+    protected double computeWeight() {
+        return this.getGrades()/20;
+    }
+    
+}
