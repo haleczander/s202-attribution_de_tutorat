6e2c17bfde2738efc3bfa0c38af8bec531516ed6
Theo Franos <theo.franos.etu@univ-lille.fr>
2022-05-18 16:52:50 +0200

    test

---
+++ src/graphs/affectation/Test.java
@@ -0,0 +1,19 @@
+package graphs.affectation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.experimental.theories.internal.Assignments;
+
+public class Test {
+    public static void main(String[] args) {
+        //Departement
+        Student std1 = new Student("A", 12.1, 2, 3);
+        Student std2 = new Student("B", 10.0, 3, 5);
+        List<Student> classe = new ArrayList<Student>();
+        Departement dep = new Departement("Test");
+        Assignment mat1 = new Assignment(classe);
+        dep.addTutoring(Resource.R101, mat1);
+        
+    }
+}
