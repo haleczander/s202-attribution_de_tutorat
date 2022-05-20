084c674093e506a23476a62051ed7df975ca3860
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-05-20 15:02:15 +0200

    petit pas de plus 3version

--- src/ihm/VersionDeux.java
+++ src/ihm/VersionDeux.java
@@ -1,32 +1,52 @@
 package ihm;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class VersionDeux extends Application{
     //Glob
-
+    Scene scene;
     Button sessionBt = new Button("Non connecté");
     ComboBox<String> cbMatieres = new ComboBox<>();
-    Image profilPhoto = new Image ("file:res/img/jc2.jpeg");
+    Image profilPhoto;
     ComboBox<String> cbSession = new ComboBox<>();
+
+    int slMin = 0;
+    int slMax = 5;
+
+    Slider slAvg = new Slider(slMin, slMax, 1);
+    Slider slAbs = new Slider(slMin, slMax, 1);
+    Slider slMot = new Slider(slMin, slMax, 1);
+
+    //Padding
+    Insets PAD_MIN = new Insets(5);
 
 
     @Override
     public void start(Stage stage) throws Exception {        
         BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 800, 500);
 
         MenuBar menu = initMenu();
         root.setTop(menu);
@@ -37,7 +57,7 @@ public class VersionDeux extends Application{
         HBox footer = initFooter();
         root.setBottom(footer);
 
-        stage.setScene(new Scene(root, 800, 500));
+        stage.setScene(scene);
         stage.show();
         
         
@@ -46,25 +66,83 @@ public class VersionDeux extends Application{
     VBox initMain(){
         VBox main = new VBox();
 
-        HBox header = new HBox();
-        cbMatieres.setPromptText("Choisir une matière");
+        HBox header = initHeader();        
 
-        HBox session = new HBox();
-        ImageView logoImgView = new ImageView(new Image("file:res/img/jc2.jpeg"));
-        ImageView sessionImgView = new ImageView(profilPhoto);
-        cbSession.setPromptText("Non connecté");
-        session.getChildren().addAll(cbSession, sessionImgView);
-
-        header.getChildren().addAll(cbMatieres, logoImgView, session);
-        
-
-        HBox options = new HBox();
+        HBox options = initOptions();
 
         HBox listes = new HBox();
 
         main.getChildren().addAll(header, options, listes);
 
         return main;
+    }
+
+    void setSlValue(Slider s, Number newVal){
+        {((Slider)s).setValue( Math.ceil((double)newVal*2) / 2.0d);}
+    }
+    
+    HBox initOptions(){
+        HBox options = new HBox();
+        options.setAlignment(Pos.CENTER);
+
+        HBox coefs = new HBox();
+        Slider[] sliders = {slAvg, slAbs, slMot};
+        for (Slider s : sliders) {
+            s.setMaxWidth(75);
+            s.setMajorTickUnit(1);
+            s.setShowTickLabels(true);
+        }
+
+        slAvg.valueProperty().addListener((obs, oldval, newVal) -> setSlValue(slAvg, newVal));
+        slAbs.valueProperty().addListener((obs, oldval, newVal) -> setSlValue(slAbs, newVal));
+        slMot.valueProperty().addListener((obs, oldval, newVal) -> setSlValue(slMot, newVal));
+
+        Label labelAvg = new Label("Moyenne");
+        Label labelAbs = new Label("Absences");
+        Label labelMot = new Label("Motivation");
+        Button resetCoef = new Button("↺");
+
+        coefs.getChildren().addAll(labelAvg, slAvg, labelAbs, slAbs, labelMot, slMot, resetCoef);
+
+
+        options.getChildren().addAll(coefs);
+        return options;
+    }
+
+    HBox initHeader(){
+        HBox header = new HBox();
+        header.setPadding(PAD_MIN);
+
+        HBox matieres = new HBox();
+        matieres.setAlignment(Pos.CENTER_LEFT);
+        cbMatieres.setPromptText("Choisir une matière");
+        matieres.getChildren().addAll(cbMatieres);
+
+
+        HBox logo = new HBox();
+        HBox.setHgrow(logo, Priority.ALWAYS);
+        logo.setAlignment(Pos.CENTER);
+
+        HBox session = new HBox();
+        session.setAlignment(Pos.CENTER_RIGHT);
+        ImageView logoImgView = new ImageView(new Image("file:res/img/logo.png"));
+        logoImgView.setFitHeight(75);
+        logoImgView.setPreserveRatio(true);
+        logo.getChildren().addAll(logoImgView);
+
+        setProfilPhoto();
+        Circle sessionPhoto = new Circle(50);
+        sessionPhoto.setStroke(Color.DARKGREY);
+        sessionPhoto.setFill(new ImagePattern(profilPhoto));
+
+        cbSession.setPromptText("Non connecté");
+        session.getChildren().addAll(cbSession, sessionPhoto);
+
+        header.getChildren().addAll(matieres, logo, session);
+        for (Node n : header.getChildren()){
+            n.minHeight(Double.MAX_VALUE);
+        }
+        return header;
     }
 
     HBox initFooter(){
@@ -149,6 +227,13 @@ public class VersionDeux extends Application{
 
         menu.getMenus().addAll(fichier, selection, listes, affectation, edition);
         return menu;
+    }
+
+    void setProfilPhoto(String path){
+        profilPhoto = new Image (path);
+    }
+    void setProfilPhoto(){
+        setProfilPhoto("file:res/img/notlogged.jpg");
     }
 
     public static void main(String[] args) {
