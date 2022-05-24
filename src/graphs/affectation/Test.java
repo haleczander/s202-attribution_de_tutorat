266d12a058e0294268338bc763451e33b6af1160
Theo Franos <theo.franos.etu@univ-lille.fr>
2022-05-24 14:19:36 +0200

    test affectation polytutorant + ajout UML dans res

--- src/graphs/affectation/Test.java
+++ src/graphs/affectation/Test.java
@@ -10,15 +10,19 @@ import fr.ulille.but.sae2_02.graphes.CalculAffectation;
 public class Test {
     public static void main(String[] args) {
         //crée Dep + ajouter étudiants + assignation tuteur / tutoré (cas ou moins de tuteur)
+    	
 //        Tutor tutor1 = new Tutor("A", 12.1, 2, 3, 'A');
 //        Tutor tutor2 = new Tutor("Jean", 13, 2, 3, 'C');
 //        Tutor tutor3 = new Tutor("Pierre", 18, 2, 3, 'A');
+    	
 //        Tutored tutored1 = new Tutored("Axel", 5, 2, 'C');
 //        Tutored tutored2 = new Tutored("Tim", 15, 2, 'C');
 //        Tutored tutored3 = new Tutored("Harry", 3, 2, 'C');
 //        Tutored tutored4 = new Tutored("Paul", 12, 2, 'C');
+    	
 //        List<Tutor> tuteur = new ArrayList<Tutor>();
 //        List<Tutored> tutoré = new ArrayList<Tutored>();
+    	
 //        tuteur.add(tutor1);
 //        tutoré.add(tutored1);
 //        tuteur.add(tutor2);
@@ -26,10 +30,51 @@ public class Test {
 //        tuteur.add(tutor3);
 //        tutoré.add(tutored3);
 //        tutoré.add(tutored4);
+    	
 //        Departement dep = new Departement("Test");
 //        Assignment mat1 = new Assignment(tutoré, tuteur);
+    	
 //        dep.addTutoring(Resource.R101, mat1);
-//        System.out.println(dep);
+    	
+//        System.out.println(dep "\n" + "\n");
+    	
 //        System.out.println(mat1.getTextAssignment());
+    	
+//    	  System.out.println(mat1.getAssignment());
+    	
+    	
+    	//cas ou le polytutorat accepté
+    	
+    	Tutor tutor1 = new Tutor("A", 12.1, 2, 3, 'A');
+        Tutor tutor2 = new Tutor("Jean", 13, 2, 3, 'C');
+        Tutor tutor3 = new Tutor("Pierre", 18, 3, 0, 'A'); //obligé d'être 3ème année pour pouvoir tutoré 2 étudiants 
+        
+        Tutored tutored1 = new Tutored("Axel", 5, 2, 'C');
+        Tutored tutored2 = new Tutored("Tim", 15, 2, 'C');
+        Tutored tutored3 = new Tutored("Harry", 3, 2, 'C');
+        Tutored tutored4 = new Tutored("Paul", 12, 2, 'C');
+        
+        List<Tutor> tuteur = new ArrayList<Tutor>();
+        List<Tutored> tutoré = new ArrayList<Tutored>();
+        
+        tuteur.add(tutor1);
+        tutoré.add(tutored1);
+        tuteur.add(tutor2);
+        tutoré.add(tutored2);
+        tuteur.add(tutor3);
+        tutoré.add(tutored3);
+        tutoré.add(tutored4);
+        
+        Departement dep = new Departement("Test");
+        Assignment mat1 = new Assignment(tutoré, tuteur);
+        mat1.togglePolyTutor();
+        mat1.setPolyTutor(true);
+        dep.addTutoring(Resource.R101, mat1);
+        
+        System.out.println(dep + "\n" + "\n");
+        
+        System.out.println(mat1.getTextAssignment());
+        
+        System.out.println(mat1.getAssignment());
     }
 }
