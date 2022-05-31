3fe7fff9693c4e04b4c5fbd44def2256f1cf2f72
Theo Franos <theo.franos.etu@univ-lille.fr>
2022-05-31 14:52:01 +0200

    Assignment Test

---
+++ tests/AssignmentTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssignmentTest {
+
+	
+	void init() {
+		Tutor t1
+		Tutor t2
+	}
+
+}
--- tests/Test.java
+++ tests/Test.java
@@ -18,7 +18,26 @@ import oop.Resource;
 
 public class Test {
     public static void main(String[] args) {
-        //Assignation tuteur / tutoré (cas ou moins de tuteur)
+       
+    //test class Assignment
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    
+    	
+    	
+    	
+    	//Tout les cas d'Assignation
+    	
+    	//Assignation tuteur / tutoré (cas ou moins de tuteur)
     	
 //        Tutor tutor1 = new Tutor("A", 12.1, 2, 3, 'A');
 //        Tutor tutor2 = new Tutor("Jean", 13, 2, 3, 'C');
@@ -178,42 +197,42 @@ public class Test {
     	
     	
     	//Affectation nombre tuteur = nombre tutoré avec polytutorat autorisé
-    	Tutor tutor1 = new Tutor("A", 12.1, 2, 3, 'A');
-        Tutor tutor2 = new Tutor("Jean", 13, 2, 3, 'C');
-        Tutor tutor3 = new Tutor("Pierre", 18, 3, 0, 'A');
-        Tutor tutor4 = new Tutor("Michel", 14, 3, 1, 'A');
-        
-        Tutored tutored1 = new Tutored("Axel", 5, 2, 'C');
-        Tutored tutored2 = new Tutored("Tim", 15, 2, 'C');
-        Tutored tutored3 = new Tutored("Harry", 3, 2, 'C');
-        Tutored tutored4 = new Tutored("Paul", 12, 2, 'C');
-        
-        List<Tutor> tuteur = new ArrayList<>();
-        List<Tutored> tutore = new ArrayList<>();
-        
-        tuteur.add(tutor1);
-        tuteur.add(tutor2);
-        tuteur.add(tutor3);
-        tuteur.add(tutor4);
-        
-        tutore.add(tutored1);
-        tutore.add(tutored2);
-        tutore.add(tutored3);
-        tutore.add(tutored4);
-        
-        
-        Departement dep = new Departement("Test");
-        Assignment mat1 = new Assignment(tutore, tuteur);
-        mat1.togglePolyTutor();
-        mat1.setPolyTutor(true);
-        dep.addTutoring(Resource.R101, mat1);
-        
-        System.out.println(dep + "\n" + "\n");
-        
-        System.out.println(mat1.getTextAssignment());
-        
-        System.out.println(mat1.getAssignment());
-    	
-    	
+//    	Tutor tutor1 = new Tutor("A", 12.1, 2, 3, 'A');
+//        Tutor tutor2 = new Tutor("Jean", 13, 2, 3, 'C');
+//        Tutor tutor3 = new Tutor("Pierre", 18, 3, 0, 'A');
+//        Tutor tutor4 = new Tutor("Michel", 14, 3, 1, 'A');
+//        
+//        Tutored tutored1 = new Tutored("Axel", 5, 2, 'C');
+//        Tutored tutored2 = new Tutored("Tim", 15, 2, 'C');
+//        Tutored tutored3 = new Tutored("Harry", 3, 2, 'C');
+//        Tutored tutored4 = new Tutored("Paul", 12, 2, 'C');
+//        
+//        List<Tutor> tuteur = new ArrayList<>();
+//        List<Tutored> tutore = new ArrayList<>();
+//        
+//        tuteur.add(tutor1);
+//        tuteur.add(tutor2);
+//        tuteur.add(tutor3);
+//        tuteur.add(tutor4);
+//        
+//        tutore.add(tutored1);
+//        tutore.add(tutored2);
+//        tutore.add(tutored3);
+//        tutore.add(tutored4);
+//        
+//        
+//        Departement dep = new Departement("Test");
+//        Assignment mat1 = new Assignment(tutore, tuteur);
+//        mat1.togglePolyTutor();
+//        mat1.setPolyTutor(true);
+//        dep.addTutoring(Resource.R101, mat1);
+//        
+//        System.out.println(dep + "\n" + "\n");
+//        
+//        System.out.println(mat1.getTextAssignment());
+//        
+//        System.out.println(mat1.getAssignment());
+//    	
+//    	
     }
 }
