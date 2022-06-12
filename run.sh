71789ced88654fe00627d193ee4710e774418913
Léopold V <5019-leopold.varlamoff.etu@users.noreply.gitlab.univ-lille.fr>
2022-06-12 19:36:55 +0200

    mini update ozisdhfkljsdhf

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-javaw --module-path ./lib/javafx-sdk-17.0.2/lib --add-modules javafx.controls,javafx.fxml -jar A-G5.jar
+#!/bin/bash
+
+java --module-path $1 --add-modules javafx.controls,javafx.fxml -jar A-G5.jar
